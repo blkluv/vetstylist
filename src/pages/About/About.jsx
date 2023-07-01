4c85657e5f56358f320defaed29f993f80a23dd5
blkluv <info@blkluv.org>
2023-07-01 14:26:36 -0400

    updated nav

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
       <div className="about-info">
         <h3>About Yvette</h3>
         <p>
-          Yvette is a master cosmetologist with 40 years of experience in the industry. She started her journey as a hair stylist in New Orleans and migrated to Atlanta.
+          Yvette is a master cosmetologist with over 40 years of experience in the industry. She started her journey as a hair stylist in New Orleans and migrated to Atlanta.
           <br /><br />
           Yvette constantly seeks new ways to expand her knowledge and improve her craft, and she is dedicated to providing her clients with the best possible service and experience.
           <br /><br />
