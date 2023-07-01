b7a836fd0d15e8e32b6fda641ebaf5fd03a681b7
blkluv <info@blkluv.org>
2023-07-01 13:47:38 -0400

    menu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,18 +1,37 @@
+import React from 'react';
 import './Nav.css';
 
 export default function Nav({ currentPage, setCurrentPage }) {
-
-    return (
-        <div className="Nav">
-            <h1 className="title-hover" onClick={() => setCurrentPage('home')}>Hair Vet</h1>
-            <ul>
-                <li className="hover" onClick={() => setCurrentPage('booking')}>Booking</li>
-                {/* <li className="hover"><a href="https://vett-102378.square.site" target="_blank" rel="noreferrer">Booking</a></li> */}
-                <li className="hover" onClick={() => setCurrentPage('dates')}>Travel Dates</li>
-                <li className="hover" onClick={() => setCurrentPage('about')}>About</li>
-                <li className="hover"><a href="https://instagram.com/hairvetatl" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a></li>
-                <li className="hover"><a href="mailto:ymtriplett@gmail.com"><i className="fa-regular fa-envelope"></i></a></li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="Nav">
+      <h1 className="title-hover" onClick={() => setCurrentPage('home')}>
+        Hair Vet
+      </h1>
+      <ul>
+        <li className="hover" onClick={() => setCurrentPage('booking')}>
+          Booking
+        </li>
+        {/* <li className="hover"><a href="https://vett-102378.square.site" target="_blank" rel="noreferrer">Booking</a></li> */}
+        <li className="hover" onClick={() => setCurrentPage('dates')}>
+          Travel Dates
+        </li>
+        <li className="hover" onClick={() => setCurrentPage('about')}>
+          About
+        </li>
+        <li className="hover">
+          <a href="https://instagram.com/hairvetatl" target="_blank" rel="noreferrer">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+        </li>
+        <li className="hover">
+          <a href="mailto:ymtriplett@gmail.com">
+            <i className="fa-regular fa-envelope"></i>
+          </a>
+        </li>
+        <li className="hover">
+          <a href="tel:6785224329">678-522-4329</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
